61d0c88b0d84b9de6a770745570458b511d83bf8
Unknown <AlixAkhribi@yahoo.com>
2019-03-22 18:50:34 -0700

    Create GoogleAuth component to handle authentication

---
+++ src/components/GoogleAuth.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+
+export default class GoogleAuth extends Component {
+    state = {
+        isSignedIn: null,
+    }
+
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '',
+                scope: 'email'
+            }).then(() => {
+                this.auth = window.gapi.auth2.getAuthInstance();
+                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                Google Auth
+            </div>
+        )
+    }
+}
