9a6599a8020a68c2cdbbb6758c41a4f32801004d
Unknown <AlixAkhribi@yahoo.com>
2019-03-27 14:36:46 -0700

    Render list of streams in StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-export default function StreamDelete() {
-    return (
-        <div>List</div>
-    )
+class StreamsList extends Component {
+    componentDidMount() {
+        this.props.fetchStreams()
+    }
+
+
+    renderStreamsList() {
+        return this.props.streams.map(stream => {
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera" />
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">
+                            {stream.description}
+                        </div>
+                    </div>
+                </div>
+            )
+        })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Streams</h2>
+                <div className="ui celled list">{this.renderStreamsList()}</div>
+            </React.Fragment>
+
+        )
+    }
 }
+
+const mapStateToProps = (state) => {
+    // Use Object.values to turn into array of objects for mapping //
+    return { streams: Object.values(state.streams) }
+}
+
+export default connect(mapStateToProps, { fetchStreams })(StreamsList)
