460e4393e8c0b7ac2bde4899d7a397ffbd07eb79
Unknown <AlixAkhribi@yahoo.com>
2019-03-26 18:51:12 -0700

    Create RESTful actions for streams API
    fetchStreams, fetchStream, editStream, deleteStream,

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -6,12 +6,13 @@ export const signIn = (userId) => {
         payload: userId
     }
 
-}
+};
+
 export const signOut = () => {
     return {
         type: "SIGN_OUT",
     }
-}
+};
 
 export const createStream = formValues => async (dispatch) => {
     const response = await streams.post('/streams', formValues);
@@ -19,5 +20,41 @@ export const createStream = formValues => async (dispatch) => {
     dispatch({
         type: "CREATE_STREAM",
         payload: response.data,
+    })
+};
+
+export const fetchStreams = () => async (dispatch) => {
+    const response = await streams.get('/streams');
+
+    dispatch({
+        type: "FETCH_STREAMS",
+        payload: response.data,
+    })
+};
+
+export const fetchStream = id => async (dispatch) => {
+    const response = await streams.get(`/stream/${id}`);
+
+    dispatch({
+        type: "FETCH_STREAM",
+        payload: response.data,
+    })
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+    const response = await streams.put(`streams/${id}`, formValues);
+
+    dispatch({
+        type: "EDIT_STREAM",
+        payload: response.data,
+    })
+}
+
+export const deleteStream = (id) => async (dispatch) => {
+    await streams.delete(`streams/${id}`);
+
+    dispatch({
+        type: "DELETE_STREAM",
+        payload: id,
     })
 }
