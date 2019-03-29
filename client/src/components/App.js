90f7d20c9bc4bbbd47c7a0134f5a08ece05f466c
Unknown <AlixAkhribi@yahoo.com>
2019-03-28 20:28:16 -0700

    Create custom history object, refactor App component to use Router and custom history object

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { Router, Route } from 'react-router-dom';
+
+import history from '../history';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamDelete from './streams/StreamDelete';
 import StreamShow from './streams/StreamShow';
-
 import Header from './Header'
 
 export default class App extends Component {
   render() {
     return (
       <div className="ui container">
-        <BrowserRouter>
+        <Router history={history}>
           <React.Fragment>
             <Header />
             <Route exact path="/" component={StreamList} />
             <Route exact path="/streams/show" component={StreamShow} />
             <Route exact path="/streams/new" component={StreamCreate} />
-            <Route exact path="/streams/edit" component={StreamEdit} />
+            <Route exact path="/streams/edit/:id" component={StreamEdit} />
             <Route exact path="/streams/delete" component={StreamDelete} />
           </React.Fragment>
-        </BrowserRouter>
+        </Router>
       </div>
     )
   }
