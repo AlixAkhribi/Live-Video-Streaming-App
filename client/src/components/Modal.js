be17474f6dbee532a66f3fa76fb2269acb1fb46c
Unknown <AlixAkhribi@yahoo.com>
2019-03-31 20:36:08 -0700

    Add Modal component props

--- client/src/components/Modal.js
+++ client/src/components/Modal.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default function Modal() {
+export default function Modal(props) {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standard modal visible active">
-                Test Text
+        <div
+            onClick={props.onDismiss}
+            className="ui dimmer modals visible active"
+        >
+            <div
+                onClick={(e) => e.stopPropagation()}
+                className="ui standard modal visible active"
+            >
+                <div className="header">{props.title}</div>
+                <div className="content">{props.description}</div>
+                <div className="actions">{props.actions}</div>
             </div>
         </div>,
         document.querySelector("#modal")
