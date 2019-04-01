925ffd30df4b1ab970b9052647c0d0820b50562f
Unknown <AlixAkhribi@yahoo.com>
2019-03-31 19:48:16 -0700

    Refactor StreamDelete to class component

--- client/src/components/streams/StreamDelete.js
+++ client/src/components/streams/StreamDelete.js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { Component } from 'react';
+import Modal from '../Modal';
 
-export default function StreamDelete() {
-    return (
-        <div>Delete</div>
-    )
+import history from '../../history';
+
+export default class StreamDelete extends Component {
+    renderActions() {
+        return (
+            <React.Fragment>
+                <button className="ui button negative">Delete</button>
+                <button className="ui button">Cancel</button>
+            </React.Fragment>
+        )
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                Delete Stream
+                <Modal
+                    title="Delete Stream"
+                    description="Are you sure you want to delete this stream?"
+                    actions={this.renderActions}
+                    onDismiss={() => history.push('/')}
+                />
+            </React.Fragment>
+        )
+    }
 }
