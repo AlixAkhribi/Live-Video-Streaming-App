6d2aa1525857f6ef9548f0cac5d88a1549533754
Unknown <AlixAkhribi@yahoo.com>
2019-04-01 16:30:04 -0700

    Title of stream is now Link in StreamList component

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -45,7 +45,9 @@ class StreamsList extends Component {
                     {this.renderPermissionsButtons(stream)}
                     <i className="large middle aligned icon camera" />
                     <div className="content">
-                        {stream.title}
+                        <Link to={`/streams/${stream.id}`} className="header">
+                            {stream.title}
+                        </Link>
                         <div className="description">
                             {stream.description}
                         </div>
