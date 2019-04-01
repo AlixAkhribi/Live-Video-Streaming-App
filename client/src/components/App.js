84f0a9053a7a34634cc150b2acf901e6ff8df5b9
Unknown <AlixAkhribi@yahoo.com>
2019-04-01 16:29:16 -0700

    Wrap routes with Switch for bug fix, change StreamShow path

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import history from '../history';
 
@@ -17,11 +17,13 @@ export default class App extends Component {
         <Router history={history}>
           <React.Fragment>
             <Header />
-            <Route exact path="/" component={StreamList} />
-            <Route exact path="/streams/show" component={StreamShow} />
-            <Route exact path="/streams/new" component={StreamCreate} />
-            <Route exact path="/streams/edit/:id" component={StreamEdit} />
-            <Route exact path="/streams/delete/:id" component={StreamDelete} />
+            <Switch>
+              <Route exact path="/" component={StreamList} />
+              <Route exact path="/streams/:id" component={StreamShow} />
+              <Route exact path="/streams/new" component={StreamCreate} />
+              <Route exact path="/streams/edit/:id" component={StreamEdit} />
+              <Route exact path="/streams/delete/:id" component={StreamDelete} />
+            </Switch>
           </React.Fragment>
         </Router>
       </div>
